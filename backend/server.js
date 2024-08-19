658051096b9e25826e86bcd65fc29470d1d8ad05
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-08-19 08:27:26 -0700

    deploying with build script web server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
-// import path from "path";
-import cors from "cors";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -14,8 +13,6 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,18 +20,18 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running....");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://mern-auth-forked.onrender.com",
+  baseUrl: "",
 });
 
 export const apiSlice = createApi({
